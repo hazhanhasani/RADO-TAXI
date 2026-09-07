77c893bb906080afd44ec1c3c529435ce58b2a91
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-08 02:11:17 +0330

    feat(cpanel): add updater configuration

---
+++ deploy/cpanel/config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'repo' => 'hazhanhasani/RADO-TAXI',
+    'domain' => 'https://rado-taxi.sbs',
+    'github_api' => 'https://api.github.com',
+    'user_agent' => 'RADO-cPanel-Updater/1.0',
+    'preserve' => ['.env', 'storage', 'rado-system/state'],
+];
