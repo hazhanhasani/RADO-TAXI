f152fd9d1c22e540614d095428ebb65cafcda9a9
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-08 03:43:33 +0330

    chore: define Tehran timezone in cPanel config

--- deploy/cpanel/config.php
+++ deploy/cpanel/config.php
@@ -4,6 +4,7 @@ return [
     'domain' => 'https://rado-taxi.sbs',
     'github_api' => 'https://api.github.com',
     'user_agent' => 'RADO-cPanel-Updater/1.1',
+    'timezone' => 'Asia/Tehran',
     'oidc_audience' => 'rado-taxi.sbs',
     'oidc_issuer' => 'https://token.actions.githubusercontent.com',
     'preserve' => ['.env', 'storage', 'rado-system/state', 'rado-system/private'],
